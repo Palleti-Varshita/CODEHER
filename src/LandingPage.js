af38d823f5faae0693951c55242891cae90efc54
Palleti-Varshita <p.varshita1105@gmail.com>
2025-01-30 06:51:45 +0000

    changes

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,44 @@
 const LandingPage = () => {
-    return (
-      <div>
-        <h1>Welcome to the Landing Page!</h1>
+  return (
+    <div>
+      <nav className="top-bar">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/d/da/GITAM_logo.svg/1200px-GITAM_logo.svg.png"
+          alt="GITAM Logo"
+          className="logo"
+        />
+        <ul>
+          <li>Home</li>
+          <li>G-Student</li>
+          <li>Explore</li>
+        </ul>
+        <img
+          src="https://github.com/Palleti-Varshita.png"
+          style={{
+            height: "48px",
+            width: "48px",
+            borderRadius: "50%",
+          }}
+        />
+      </nav>
+      <div className="container">
+        <div className="goddess-container">
+          <div className="circular-text">
+            She Talks Tech - Empowering Women in Technology
+          </div>
+          <img
+            src="df-removebg-preview.png"
+            alt="She Talks Tech"
+            className="goddess-image"
+          />
+        </div>
+
+        <div className="login-box">
+          <a href="dashboard.html">Get Started - Login/Sign Up</a>
+        </div>
       </div>
-    );
-  };
-  
-  export default LandingPage;
-  
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage'; // Ensure this path is correct
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./LandingPage"; // Ensure this path is correct
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+import App from './App'
+
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
